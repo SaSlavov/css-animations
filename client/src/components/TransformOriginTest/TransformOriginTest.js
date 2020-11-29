da4358f8f5f7856a844d9920ac22c392d431eca4
Slavomir Slavov <slavomir.slavov.a20@learn.telerikacademy.com>
2020-11-29 22:58:32 +0200

    add blue-white card

---
+++ client/src/components/TransformOriginTest/TransformOriginTest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransformOriginTest = () => {
+    return (
+        <div className="test-container">
+            <div className="origin-test"></div>
+        </div>
+    );
+};
+
+export default TransformOriginTest;
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import BlueWhiteCard from '../../components/BlueWhiteCard/BlueWhiteCard';
 import HeadphonesCard from '../../components/HeadphonesCard/HeadphonesCard';
 import RedWhiteCard from '../../components/RedWhiteCard/RedWhiteCard';
 import StackedCards from '../../components/StackedCards/StackedCards';
 import './Homepage.css'
 
 const Homepage = () => {
-    
+
     return (
         <div id="home-container">
             <HeadphonesCard />
             <StackedCards />
             <RedWhiteCard />
+            <BlueWhiteCard />
+            
         </div>
     );
 };
