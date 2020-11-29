271972a58e27ffc8b4f32256ec5c698ff5b5384b
Slavomir Slavov <slavomir.slavov.a20@learn.telerikacademy.com>
2020-11-29 13:19:39 +0200

    add headphones card

---
+++ client/src/components/HeadphonesCard/HeadphonesCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HeadphonesCard.css'
+import headphones from '../../images/headphones.png'
+
+const HeadphonesCard = () => {
+    return (
+        <div className="card">
+            <div className="img-box">
+                <img src={headphones} alt="headphones" />
+            </div>
+            <div className="contentBx">
+                <h3>Wireless Headphones</h3>
+                <h2 className="price">$40.<small>99</small></h2>
+                <a className="buy" href="#">Buy Now</a>
+            </div>
+        </div>
+    );
+};
+
+export default HeadphonesCard;
